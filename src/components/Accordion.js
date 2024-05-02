62fb1589f88b415a4c014c15c41d8d83954fb92d
Rudraksh Joshi <rudrakshjoshi27@gmail.com>
2024-05-02 22:00:29 +0530

    now only one item can be opened in accordian at once

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,17 +1,33 @@
 import Item from "./Item";
+import { useState } from "react";
 
 export default function Accordion({ faqs }) {
+  const [currOpen, setCurrOpen] = useState(0);
+
   return (
     <div className="accordion">
       {faqs.map((faq, index) => (
         <Item
-        key = {Date.now() + Math.random()}
+          key={Date.now() + Math.random()}
           title={faq.title}
-          text={faq.text}
           number={index + 1}
-          open={faqs.open}
-        />
+          onOpen={setCurrOpen}
+          currentOpen={currOpen}
+        >
+          {faq.text}
+        </Item>
       ))}
+      <Item
+        title="Thinking in React"
+        number={23}
+        onOpen={setCurrOpen}
+        currentOpen={currOpen}
+      >
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur
+        soluta hic sapiente expedita, adipisci, alias, voluptatum laudantium
+        optio praesentium magnam aperiam ullam iusto ipsam reiciendis dolorum
+        facere iure pariatur? Culpa.
+      </Item>
     </div>
   );
 }
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,19 +1,17 @@
-import { useState } from "react";
-
-export default function Item({ title, text, number }) {
-  const [isOpen, setIsOpen] = useState(false);
-  function handleIsOpen() {
-    setIsOpen((isOpen) => !isOpen);
+export default function Item({ title, children, number, onOpen, currentOpen }) {
+  const isOpen = number === currentOpen;
+  function handleToggle() {
+    onOpen((currOpen) => (currOpen === 0 ? number : 0));
   }
   return (
-    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleIsOpen}>
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
       <span className="number">0{number}</span>
       <p className="title">{title}</p>
 
       {isOpen ? (
         <>
           <p className="icon">-</p>
-          <p className="content-box ">{text}</p>
+          <p className="content-box ">{children}</p>
         </>
       ) : (
         <>
